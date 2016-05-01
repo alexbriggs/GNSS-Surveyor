4af1b62a9095f3c81a5d6fc3fa27fce000896ef2
alex briggs <alexbriggs@live.co.uk>
2016-05-01 21:39:01 +0100

    Licencing and comments completed. Code ran and checked to confirm functionality has been achieved and code is stable.

--- GNSS Surveyor/BingMaps.xaml.cs
+++ GNSS Surveyor/BingMaps.xaml.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿/// <summary>
+/// BingMaps.xaml.cs - Mapping user control implementing Bing Maps WPF api. Stores and exports geographical data.
+//Copyright(C) 2016  Alexander Briggs
+
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program.If not, see<http://www.gnu.org/licenses/>.
+/// </summary>
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,6 +33,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Maps.MapControl.WPF;
 using System.Reflection;
+
 using System.IO;
 
 namespace GNSS_Surveyor
